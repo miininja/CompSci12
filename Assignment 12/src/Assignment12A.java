cd36275a73bc83e6500a9dba143ac2813932deb6
miininja <80184913+miininja@users.noreply.github.com>
2021-05-05 11:43:29 -0300

    Add files via upload

---
+++ Assignment 12/src/Assignment12A.java
@@ -0,0 +1,9 @@
+class Assignment12A
+{
+	public static void main (String[] args)
+	{
+		Athlete person = new Athlete("Amy","defense",21,5);
+		person.displayAthlete();
+		person.changeVars();
+	}
+}
